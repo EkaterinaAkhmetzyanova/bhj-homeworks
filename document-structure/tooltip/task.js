df7e995af0562f801d0cd91c6b1a3d696e775c73
Ekaterina Akhmetzyanova <a.ekaterina323@gmail.com>
2021-03-19 21:49:06 +0300

    hw 2.3 task 1 - v.3

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -6,17 +6,19 @@ for (let i = 0; i < tooltipElements.length; i++) {
     tooltipElement.addEventListener("click", showTip);
 
     function showTip(event) {
+        const title = event.target.getAttribute("title");
         if (activeTooltip !== null) {
             activeTooltip.classList.remove("tooltip_active");
+            activeTooltip = null;
+            event.preventDefault();
             return false;
         }
-        const title = event.target.getAttribute("title");
         const tooltipDiv = document.createElement("div");
         tooltipDiv.className = "tooltip";
-        tooltipElement.insertAdjacentElement("afterend", tooltipDiv);
         tooltipDiv.innerText = title;
+        tooltipElement.insertAdjacentElement("afterend", tooltipDiv);
         tooltipDiv.classList.add("tooltip_active");
-        activeTooltip = document.querySelector(".tooltip");
+        activeTooltip = document.querySelector(".tooltip_active");
         tooltipDiv.style.top = event.target.getBoundingClientRect().top + 20 + "px";
         tooltipDiv.style.left = event.target.getBoundingClientRect().left + "px";
         event.preventDefault();
